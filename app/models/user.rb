30908f633334a8cf1263f13830e093d14ccdbc15
Chenyun Zhang <morningchenyun@gmail.com>
2020-09-03 22:58:34 -0400

    modified the regex for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
     has_many :questions, dependent: :destroy
     # accepts_nested_attributes_for :questions, allow_destroy: true, reject_if: :all_blank
-    before_save {self.email = email.downcase }
+    before_save {self.email = email.downcase } #or { email.downcase!}
     validates :first_name, :last_name, presence: true, length: { maximum: 50 }
     validates :email, presence: true,
                     length: { maximum: 50 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i},
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i},
                     uniqueness: { case_sensitive: false }
     has_secure_password
-    validates :password, length: { minimum: 6 }
+    validates :password, length: { minimum: 6, message: "password minimum length is 6"}
 ##############################################################
     def full_name
         "#{self.first_name} #{self.last_name}"
