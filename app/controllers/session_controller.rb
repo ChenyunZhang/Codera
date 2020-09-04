5210dfb688b666794b75f1678c6e5b50e0aab725
Chenyun Zhang <morningchenyun@gmail.com>
2020-09-04 11:41:59 -0400

    finishing up the setup form

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,9 +3,18 @@ class SessionController < ApplicationController
   end
 
   def create
-
+    #use find_by instead of find because find raise exception error when user is nil, find_by will just return nil
+    user = User.find_by(email: params[:session][:email].downcase)
+    # if the email is in the db, verify the password
+    if user && user.authenticate(params[:session][:password])
+      redirect_to user
+    else
+      flash[:danger] = "Invalid email/password"
+      redirect_to "/login"
+    end
   end
 
   def destroy
+
   end
 end
